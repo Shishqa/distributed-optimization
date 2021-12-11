593ea42b915c0735a6d8af85e6e893f7972420cd
Mikhail Shishatskiy <shishatskii.mn@phystech.edu>
2021-12-11 23:44:10 +0300

    refactor DANE

--- optimizers/DANE.py
+++ optimizers/DANE.py
@@ -4,74 +4,72 @@ import torch.distributed as dist
 
 class DANE(torch.optim.Optimizer):
 
-    def __init__(self, params, local_opt, lr=1, mu=0.5):
+    def __init__(self, params, local_opt, local_n_epochs=10, lr=1, mu=0.5):
 
-        # Get number of agents
         self.world_size = dist.get_world_size()
+        self.rank = dist.get_rank()
+        self.local_opt = local_opt
+        self.local_n_epochs = local_n_epochs
 
-        defaults = dict(local_opt=local_opt, lr=lr, mu=mu)
+        defaults = dict(lr=lr, mu=mu)
         super(DANE, self).__init__(params, defaults)
 
-        for group in self.param_groups:
-            self.reduce_params(group['params'])
+        self.reduce_params()
 
     def step(self, closure):
+        # update df_i(w(t-1))
+        loss = closure()
+        self.zero_grad()
+        loss.backward()
+
+        # 1) calculate global gradient and distribute:
+        #    df(w(t-1)) = 1/m sum{ df_i(w(t-1)) }
+        self.reduce_grads()
+
+        # 2) solve local optimization problem -> w_i(t)
+        self.local_optimize(closure)
+
+        # 3) calculate new parameter:
+        #    w(t) = 1/m sum{ w_i(t) }
+        self.reduce_params()
+
+    def sync_tensor(self, local):
+        dist.reduce(local, dst=0, op=dist.ReduceOp.SUM)
+        if self.rank == 0:
+            local /= self.world_size
+        dist.broadcast(local, src=0)
+
+    def reduce_grads(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    return
+
+                # p.grad = df(p(t-1))
+                p_state = self.state[p]
+
+                # save df(p(t-1))
+                p_state['local_grad'] = p.grad.data.detach().clone()
+
+                self.sync_tensor(p.grad.data)
+
+                p_state['global_grad'] = p.grad.data.detach().clone()
+
+    def reduce_params(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                self.sync_tensor(p.data)
+
+                p_state = self.state[p]
+                p_state['global_p'] = p.data.detach().clone()
+
+    def local_loss(self, loss):
         for group in self.param_groups:
 
             mu = group['mu']
             lr = group['lr']
-            opt = group['local_opt']
-            params = group['params']
 
-            # update df_i(w(t-1))
-            loss = closure()
-            self.zero_grad()
-            loss.backward()
-
-            # 1) calculate global gradient and distribute:
-            #    df(w(t-1)) = 1/m sum{ df_i(w(t-1)) }
-            self.reduce_grads(params)
-
-            # 2) solve local optimization problem -> w_i(t)
-            self.local_optimize(params, opt, closure, mu, lr)
-
-            # 3) calculate new parameter:
-            #    w(t) = 1/m sum{ w_i(t) }
-            self.reduce_params(params)
-
-    def reduce_grads(self, params):
-        # print('{}: reducing gradient'.format(self.rank))
-        for p in params:
-            if p.grad is None:
-                return
-
-            # p.grad = df(p(t-1))
-            p_state = self.state[p]
-
-            # save df(p(t-1))
-            p_state['local_grad'] = p.grad.data.detach().clone()
-
-            dist.all_reduce(p.grad.data, op=dist.ReduceOp.SUM)
-            p.grad.data /= self.world_size
-
-            p_state['global_grad'] = p.grad.data.detach().clone()
-            p_state['global_p'] = p.data.detach().clone()
-
-    def reduce_params(self, params):
-        # print('{}: reducing param'.format(self.rank))
-        for p in params:
-            dist.all_reduce(p.data, op=dist.ReduceOp.SUM)
-            p.data /= self.world_size
-
-    def local_optimize(self, params, opt, closure, mu, lr):
-        # print('{}: begin local optimization'.format(self.rank))
-        local_opt = opt(params, lr=0.1)
-
-        for e in range(10):
-
-            loss = closure()
-
-            for p in params:
+            for p in group['params']:
                 if p.grad is None:
                     continue
 
@@ -90,8 +88,15 @@ class DANE(torch.optim.Optimizer):
 
                 loss -= scal
 
-            local_opt.zero_grad()
-            loss.backward()
-            local_opt.step()
+        return loss
 
-            # print('{}: epoch {} loss {}'.format(self.rank, e, loss.item()))
+    def local_optimize(self, closure):
+        for e in range(self.local_n_epochs):
+            loss = closure()
+            loss = self.local_loss(loss)
+
+            self.zero_grad()
+            loss.backward()
+            self.local_opt.step()
+
+            print('{}: epoch {} loss {}'.format(self.rank, e, loss.item()))
